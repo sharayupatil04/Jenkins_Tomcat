8d8053e7eecebe8116fac077016d6391f3aeec6c
sharayupatil04 <105849189+sharayupatil04@users.noreply.github.com>
2024-08-30 12:46:55 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+  public static method() {
+    System.out.prinltn("Hello Java Method");
+    System.out.println("Hello Changed Java Method");
+  }
+  public static void main(String args[]) {
+    System.out.prinltn("Hello Java World");
+    method();
+  }
+}
