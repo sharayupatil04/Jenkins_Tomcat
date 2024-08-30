b9be629e05658ee5fec5af5d7ccc76f2af8a6ab3
sharayupatil04 <patilsharayu2002@gmail.com>
2024-08-30 15:14:21 +0530

    spelling correct

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,16 +1,16 @@
 public class HelloWorld {
 
   public static void method() {
-    System.out.prinltn("Hello Java Method");
+    System.out.println("Hello Java Method");
     System.out.println("Hello Changed Java Method");
   }
 
   public static void method2() {
-    System.out.prinltn("Hello Java Method2");
+    System.out.println("Hello Java Method2");
   }
 
   public static void main(String args[]) {
-    System.out.prinltn("Hello Java World");
+    System.out.println("Hello Java World");
     method();
     method();
   }
