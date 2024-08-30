8683319243b1fa6fb304f5261dfb665dd465b8c1
sharayupatil04 <patilsharayu2002@gmail.com>
2024-08-30 13:57:31 +0530

    Added new method

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,8 +4,14 @@ public class HelloWorld {
     System.out.prinltn("Hello Java Method");
     System.out.println("Hello Changed Java Method");
   }
+
+  public static method2() {
+    System.out.prinltn("Hello Java Method2");
+  }
+
   public static void main(String args[]) {
     System.out.prinltn("Hello Java World");
+    method();
     method();
   }
 }
